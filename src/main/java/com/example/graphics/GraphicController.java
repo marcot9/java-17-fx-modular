0eb8bfe67364e27308ec8e80ea2d96595375e6cd
Marco <mtschachtli.mt@gmail.com>
2021-12-10 10:48:45 +0100

    add GraphicController

--- src/main/java/com/example/graphics/GraphicController.java
+++ src/main/java/com/example/graphics/GraphicController.java
@@ -1,18 +1,11 @@
 package com.example.graphics;
 
-import com.example.calculator.Calculator;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class GraphicController {
     GraphicController graph = new GraphicController();
